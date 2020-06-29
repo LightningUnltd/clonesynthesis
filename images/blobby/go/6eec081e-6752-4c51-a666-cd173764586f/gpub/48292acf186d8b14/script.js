4531fc3402945f190838cda0bf9d71c422b48966
Lightning Unlimited <hello@lightningunlimited.com>
2020-06-29 13:36:13 +0530

    Initial commit

---
+++ images/blobby/go/6eec081e-6752-4c51-a666-cd173764586f/gpub/48292acf186d8b14/script.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-1',
+          componentName:'DynamicFontScaler',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:""
+        });
+      })({"font":"primary","text":"Clone Synthesis","containerId":"tagline-container-178576","targetId":"dynamic-tagline-178577","fontSizes":["xxxlarge","xxlarge","xlarge"],"maxLines":4,"style":{"display":"inline-block","maxWidth":"100%","whiteSpace":"pre-line","borderBottomWidth":"2px","borderBottomStyle":"solid","borderColor":"highContrastOverlay","paddingBottom":"xsmall","@sm":{"borderBottomWidth":0,"paddingBottom":0},"@xs-only":{"maxWidth":"100%","marginBottom":"xsmall","wordWrap":"break-word","overflowWrap":"break-word"}},"typography":"HeadingAlpha","widgetId":"8f767218-764d-4dd5-801e-8d87cb9f6d2d","section":"overlay","category":"accent","locale":"en-IN","renderMode":"PUBLISH"},{"widgetId":"8f767218-764d-4dd5-801e-8d87cb9f6d2d","widgetType":"HEADER","widgetPreset":"header9","section":"overlay","category":"accent","fontSize":"xxxlarge","fontFamily":"primary","group":"Hero","groupType":"Default","themeOverrides":{}});
